2777dddbd068fde07bfa7c12581bf020884eb6b8
DeMineArchiver <minearchiver@gmail.com>
2024-01-09 13:45:02 +0300

    Improvements

--- apps/extension/src/App.tsx
+++ apps/extension/src/App.tsx
@@ -1,6 +1,9 @@
-import { Component } from "solid-js";
+import { Component, ParentComponent, createEffect, createSignal } from "solid-js";
 import styles from "./App.module.css";
 import { QuickLinks } from "./components/quick-links";
+import { useMousePosition } from "@solid-primitives/mouse";
+import { createElementBounds } from "@solid-primitives/bounds";
+import { makeTimer } from "@solid-primitives/timer";
 
 
 
@@ -19,19 +22,90 @@ const links = [
   "https://vanilla-extract.style",
 ];
 
+const Card: ParentComponent = (props) => {
+  const [rotateX, setRotateX] = createSignal(0);
+  const [rotateY, setRotateY] = createSignal(0);
 
+  let [cardRef, setCardRef] = createSignal<HTMLElement>();
+
+  const mouse = useMousePosition();
+
+  createEffect(() => {
+    if(mouse.isInside) {
+      const rect = cardRef()!.getBoundingClientRect();
+
+      const x = rect.left;
+      const y = rect.top;
+      const width = rect.width;
+      const height = rect.height;
+
+      const centerX = x + width / 2;
+      const centerY = y + height / 2;
+
+      const offsetX = (mouse.x - centerX) / centerX;
+      const offsetY = (mouse.y - centerY) / centerY;
+
+      setRotateX(offsetY * -25);
+      setRotateY(offsetX * 25);
+    } else {
+      setRotateX(0);
+      setRotateY(0);
+    }
+  });
+
+  return (
+    <div
+      ref={setCardRef}
+      class={styles["card"]}
+      style={{
+        "--rotateX": `${rotateX()}deg`,
+        "--rotateY": `${rotateY()}deg`,
+      }}
+      >
+      {props.children}
+    </div>
+  );
+}
+
+const getTime = () => {
+  const now = new Date();
+  const components = [
+    now.getHours(),
+    now.getMinutes(),
+    now.getSeconds(),
+  ];
+  return components
+    .map(value => value.toString().padStart(2, "0"),)
+    .join(" : ");
+}
 const App: Component = () => {
+  const [time, setTime] = createSignal(getTime());
+
+  makeTimer(
+    () => setTime(getTime()),
+    1000,
+    setInterval,
+  );
+
   return (
     <main class={styles["app"]}>
       <img
         class={styles["background"]}
         src="/images/wallpapers/glow/img22.jpg" />
       <article>
-        00 : 00 : 00
+        <span
+          class={styles["time"]}>
+          {time()}
+        </span>
       </article>
       <article>
         <QuickLinks links={links} />
       </article>
+      {/* <article>
+        <Card>
+          <QuickLinks links={links} />
+        </Card>
+      </article> */}
     </main>
   )
 }
--- apps/extension/src/vars.css.ts
+++ apps/extension/src/vars.css.ts
@@ -6,5 +6,8 @@ globalStyle(":root", {
     "--easing-emphasized": createLinear(emphasized, 100),
     "--easing-emphasized-decelerate": "cubic-bezier(0.05, 0.7, 0.1, 1.0)",
     "--easing-emphasized-accelerate": "cubic-bezier(0.3, 0.0, 0.8, 0.15)",
+    "--easing-standard": "cubic-bezier(0.2, 0.0, 0, 1.0)",
+    "--easing-standard-decelerate": "cubic-bezier(0, 0, 0, 1)",
+    "--easing-standard-accelerate": "cubic-bezier(0.3, 0, 1, 1)",
   },
 });
